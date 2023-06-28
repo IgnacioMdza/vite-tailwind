a204e034a5c3d4d6064296c27bd1f5949cff3ed1
IgnacioMdza <mdza.ignacio@gmail.com>
2023-06-28 16:55:00 -0600

    Renderizado de lista y responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Card from "./components/Card"
+import {viteFeatures} from "./data/viteFeatures"
 
 export default function App() {
   return (
     <>
       <div className='bg-neutral-900 p-8 font-sans min-h-screen'>
-        <header className='max-w-5xl mx-auto flex justify-between items-center mt-8 mb-12'>
-          <div className='flex flex-col gap-3 w-2/4'>
+        <header className='max-w-5xl mx-auto flex flex-col-reverse md:flex-row justify-between items-center mt-8 mb-12'>
+          <div className='flex flex-col gap-3 md:w-2/4 md:items-start items-center'>
             <div>
               <h1 className='inline-block font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 '>Vite</h1>
             </div>
-            <h2 className=' text-neutral-300 font-bold text-5xl'>Next Generation Frontend Tooling</h2>
-            <h3 className=' text-neutral-500 font-medium text-2xl'>Get ready for a development environment that can finally catch up with you.</h3>
-            <div className='flex gap-3 mt-4'>
+            <h2 className='text-center md:text-left text-neutral-300 font-bold text-5xl'>Next Generation Frontend Tooling</h2>
+            <h3 className='text-center md:text-left text-neutral-500 font-medium text-2xl'>Get ready for a development environment that can finally catch up with you.</h3>
+            <div className='flex justify-around sm:justify-center md:justify-start sm:gap-3 mt-4 w-full'>
               <button className='text-sm font-medium text-white bg-neutral-600 pt-2 pb-2 pl-3 pr-3 rounded-3xl hover:bg-indigo-500'>
                 Get Started
               </button>
@@ -28,13 +29,15 @@ export default function App() {
             <div className='mx-auto rotate-45 opacity-70 w-72 h-72 rounded-full bg-gradient-to-r from-blue-400 from-50% to-purple-600 to-50% blur-[45px]'></div>
           </div>
         </header>
-        <section className='grid grid-cols-3 gap-3.5 max-w-5xl mx-auto'>
-          <Card icon='💡' title='Instant Server Start' content='On demand file serving over native ESM, no bundling required!'/>
-          <Card icon='⚡️' title='Lightning Fast HMR' content='Hot Module Replacement (HMR) that stays fast regardless of app size.'/>
-          <Card icon='🛠️' title='Rich Features' content='Out-of-the-box support for TypeScript, JSX, CSS and more.'/>
-          <Card icon='📦' title='Optimized Build' content='Pre-configured Rollup build with multi-page and library mode support.'/>
-          <Card icon='🔩' title='Universal Plugins' content='Rollup-superset plugin interface shared between dev and build.'/>
-          <Card icon='🔑' title='Fully Typed APIs' content='Flexible programmatic APIs with full TypeScript typing.'/>
+        <section className='grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-3.5 max-w-5xl mx-auto'>
+          {viteFeatures.map((feature, index) =>
+            <Card
+              key={`feature-card-${index}`}
+              icon={feature.icon}
+              title={feature.title}
+              content={feature.content}
+            />
+          )}
         </section>
       </div>
     </>
